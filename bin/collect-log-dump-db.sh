37ff3de2657a44e7f127a097932918a1507da072
Thiago Manel <thiago.manel@gmail.com>
2017-04-27 10:37:26 -0300

    refactor dump collection

--- bin/collect-log-dump-db.sh
+++ bin/collect-log-dump-db.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-# This function get result execution
-function getResultExecution {
-  EXECUTION_UUID=$1
-
-  for i in $n_images
-  do
-    image_name=$(echo | getImageName $i $CURRENT_SAMPLE)
-
-    if [ ! -d $outputs_dir_path/$image_name ]
-    then
-      mkdir $outputs_dir_path/$image_name
-    fi
-
-    echo "Getting $image_name result files"
-    path_output_logs=$outputs_dir_path/$EXECUTION_UUID"_"$image_name"_sample_"$CURRENT_SAMPLE
-    mkdir $path_output_logs
-    scp -r $crawler_user_name@$crawler_ip:$crawler_outputs_dir/$image_name"/temp-worker*" $path_output_logs
-    echo "Finished $image_name result files"
-  done
-}
-
 # This function do a pg_dump of catalog database
 function getDatabaseDump {
-  EXECUTION_UUID=$1
 
   # Setting password to access db
   file="$HOME/.pgpass"
@@ -39,7 +17,7 @@ function getDatabaseDump {
   echo "$scheduler_ip:$scheduler_db_port:$sebal_db_name:$sebal_db_user:$sebal_db_password" >> $file
   chmod 0600 "$file"
 
-  sample_dir_path=$outputs_dir_path/$EXECUTION_UUID"_"$image_name"_sample_"$CURRENT_SAMPLE
+  sample_dir_path=$outputs_dir_path/
   if [ ! -d $sample_dir_path ]
   then
     mkdir -p $sample_dir_path
@@ -50,22 +28,4 @@ function getDatabaseDump {
   pg_dump -h $scheduler_ip -U $sebal_db_user -f $scheduler_db_dump
 }
 
-# This function checks if previous process executed successfully
-function checkProcessOutput {
-  PROCESS_OUTPUT=$?
-
-  if [ $PROCESS_OUTPUT -ne 0 ]
-  then
-    exit $PROCESS_OUTPUT
-  fi
-}
-
-function collectLogDumpDB {
-  CURRENT_SAMPLE=$1
-  n_images=$2
-  EXECUTION_UUID=$3
-  getResultExecution $EXECUTION_UUID
-  checkProcessOutput
-  getDatabaseDump $EXECUTION_UUID
-  checkProcessOutput
-}
+getDatabaseDump
